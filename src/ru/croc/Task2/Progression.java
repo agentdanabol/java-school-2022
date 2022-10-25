57f95d36880ec6457ba868bc6e6cc6446f39b17e
Danil <Sheykodanil@gmail.com>
2022-10-26 01:43:08 +0300

    Добавил задание 2

---
+++ src/ru/croc/Task2/Progression.java
@@ -0,0 +1,16 @@
+package ru.croc.Task2;
+
+public class Progression {
+    public static void main(String[] args) {
+        int start = Integer.parseInt(args[0]);
+        int difference = Integer.parseInt(args[1]);
+        int count = Integer.parseInt(args[2]);
+
+        int sum = start;
+        for(int i = 1; i < count; i++) {
+            start = start + difference;
+            sum += start;
+        }
+        System.out.println("Сумма прогрессии: " + sum);
+    }
+}
